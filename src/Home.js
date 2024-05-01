aea7550005284de6aa20a7833455b166272e65df
KnightTheCoder <knight14animator@gmail.com>
2024-05-01 20:59:46 +0200

    Lesson 15 completed

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ]);
 
+  const [name, setName] = useState('mario');
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
@@ -15,8 +17,8 @@ const Home = () => {
 
   useEffect(() => {
     console.log('useEffect ran');
-    console.log(blogs);
-  });
+    console.log(name);
+  }, [name]);
 
   return (
     <div className="home">
@@ -25,6 +27,8 @@ const Home = () => {
         title="All blogs!"
         handleDelete={handleDelete}
       />
+      <button onClick={() => setName('luigi')}>change name</button>
+      <p>{name}</p>
     </div>
   );
 };
