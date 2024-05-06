05bbdb813de73df960258073a68ead79be19ff3e
KnightTheCoder <knight14animator@gmail.com>
2024-05-06 11:29:57 +0200

    theme switcher added

--- Website/src/Navbar.js
+++ Website/src/Navbar.js
@@ -1,12 +1,28 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const [isDark, setIsDark] = useState(false);
+
+  const handleThemeSwitch = () => {
+    setIsDark((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (isDark) {
+      document.body.className = 'dark';
+    } else {
+      document.body.className = '';
+    }
+  }, [isDark]);
+
   return (
     <nav className="navbar">
       <h1>The Dojo Blog</h1>
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/create">New Blog</Link>
+        <button onClick={handleThemeSwitch}></button>
       </div>
     </nav>
   );
